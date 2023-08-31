e7a89e39461c0199ebdbda3cf0ef2f13ff6890be
Kiedae <ernestwilliams335@gmail.com>
2023-08-31 00:55:45 -0400

    started framework on the api request based on the users city input, added css styling

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,20 @@
-var cityInput = document.getElementById("cityname");
-var submitButton = document.getElementById('submit');
-console.log(cityInput);
+const apiKey = '210247e7072d71d9bd213f7367003a6a';
+const baseUrl = 'https://api.openweathermap.org/data/2.5';
+var cityInput = document.querySelectorAll("#city-input");
 
-submitButton.addEventListener('click', function() {
-    var cityName = cityInput.value;
-    localStorage.setItem('recent-city', cityName);
-});
+
+function fetchCity(cityName){
+var search = cityInput.value.trim();
+
+fetch(`${baseUrl}/forecast?q=${cityName}&appid=${apiKey}`);
+if (response.status === 200) {
+    return response.json();
+} else {
+    console.error("Invalid city input");
+    return null;
+}
+    
+
+
+
+}
