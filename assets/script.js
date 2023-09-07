f77be5fe122a672fc5bf1d264855dce5a40dd5de
Kiedae <ernestwilliams335@gmail.com>
2023-09-06 20:51:38 -0400

    added 5 day forecast fetch and render

--- assets/script.js
+++ assets/script.js
@@ -14,6 +14,7 @@ function handleSubmit(event) {
             if (data) {
                 console.log("Weather data:", data);
                 renderTodayWeather(data);// TODO call a function to display the data 
+                fetchFiveDayForecast(cityName);
             }
         });
 }
@@ -40,6 +41,10 @@ function fetchCity(cityName) {
 
 function renderTodayWeather(data) {
     var currentDayBox = document.getElementById('current-weather');
+    
+    // Clear previous current weather data
+    currentDayBox.innerHTML = '';
+    
     var currentCity = document.createElement('h2');
     var currentDayDate = document.createElement('h3');
     var currentTemp = document.createElement('p');
@@ -62,3 +67,59 @@ function renderTodayWeather(data) {
 }
 
 renderTodayWeather();
+
+
+function fetchFiveDayForecast(cityName) {
+    var apiUrl = `${baseUrl}/forecast?q=${cityName}&units=imperial&appid=${apiKey}`;
+
+    // Fetch the 5-day forecast data
+    return fetch(apiUrl)
+        .then((response) => {
+            if (response.status === 200) {
+                return response.json();
+            } else {
+                console.error("Error fetching 5-day forecast data");
+                return null;
+            }
+        })
+        .then((data) => {
+            if (data) {
+                console.log("5-Day Forecast data:", data);
+                renderFiveDayForecast(data);
+            }
+        })
+        .catch((error) => {
+            console.error("Error fetching 5-day forecast data:", error);
+        });
+}
+
+function renderFiveDayForecast(data) {
+    var forecastBox = document.getElementById('forecast');
+    forecastBox.innerHTML = ''; // clears previous forecast data
+    
+    // Loop through the 5-day forecast data and render each day's information
+    for (let i = 0; i < data.list.length; i += 8) { // Data is provided in 3-hour intervals, so we use 8 to get daily data
+        var forecastDay = document.createElement('div');
+        forecastDay.classList.add('forecast-day');
+        
+        var date = dayjs(data.list[i].dt * 1000); // Convert timestamp to date
+        var dateElement = document.createElement('h3');
+        dateElement.textContent = date.format('MM-DD-YYYY');
+        
+        var tempElement = document.createElement('p');
+        tempElement.textContent = `Temperature: ${data.list[i].main.temp}°F`;
+        
+        var windElement = document.createElement('p');
+        windElement.textContent = `Wind Speed: ${data.list[i].wind.speed} mph`;
+        
+        var humidityElement = document.createElement('p');
+        humidityElement.textContent = `Humidity: ${data.list[i].main.humidity}%`;
+        
+        forecastDay.appendChild(dateElement);
+        forecastDay.appendChild(tempElement);
+        forecastDay.appendChild(windElement);
+        forecastDay.appendChild(humidityElement);
+        
+        forecastBox.appendChild(forecastDay);
+    }
+}
